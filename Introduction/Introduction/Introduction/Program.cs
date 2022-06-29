6eecf7065c43ef35d6ac0225c9ffb0030d7d46b0
Azer444 <Azerrh@code.edu.az>
2022-06-29 17:53:31 +0400

    Introduction

---
+++ Introduction/Introduction/Introduction/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //3 ve 7 bolunenler
+
+            //int n = 63;
+            //if(n%3==0&&n%7==0)
+            //    Console.WriteLine(true);
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+
+
+
+            // cut ededlerin toplanmasi
+
+            //int n = 14;
+            //int m = 16;
+            //int sum= 0;
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    sum = n + m;
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+            //n ve m ededleri arasindaki tek ededlerin cemi
+
+            //int n = 1;
+            //int m = 10;
+            //int sum = 0;
+            //for (int i = n ; i < m; i++)
+            //{
+            //    if (i % 2 == 1 && n < m)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+
+            //arreyin icindeki tek ededlerin cemi
+
+            //int[] num = { 6, 7, 9, 12, 14, 19, 22, 25, 27, 28, 30 };
+            //int sum = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    if (num[i] % 2 == 1)
+            //    {
+            //        sum += num[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+            //arreyin icindeki cut edelerin sayi
+
+            //int[] num = { 6, 7, 9, 12, 14, 19, 22, 25, 27, 28, 30 };
+            //int count = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    if (num[i]%2==0)
+            //    {
+            //        count++;
+            //    }
+
+                
+            //}
+            //Console.WriteLine(count);
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+    
+}
